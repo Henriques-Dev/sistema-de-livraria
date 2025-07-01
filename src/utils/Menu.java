d4a0bb3339bb8472a74aa933fd65e1fb81945aec
Henriques-Dev <lucaschenriques@icloud.com>
2025-07-01 20:09:40 -0300

    feat: Criação do menu interativo no pacote utils

---
+++ src/utils/Menu.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class Menu {
+    public static void mostrarMenu() {
+        System.out.println("\n==== MENU LIVRARIA ====");
+        System.out.println("[1] - Cadastrar Livro");
+        System.out.println("[2] - Listar Livros Disponíveis");
+        System.out.println("[3] - Pegar Livro Emprestado");
+        System.out.println("[4] - Devolver Livro");
+        System.out.println("[5] - Deletar Livro");
+        System.out.println("[6] - Sair (Encerrar Programa)");
+        System.out.println();
+        System.out.print("Digite a opção desejada: ");
+    }
+}
